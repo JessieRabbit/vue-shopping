ca25ccae409b3b77ed83386987640e44b5f269d4
JessieRabbit <a0915655212@gmail.com>
2024-04-26 20:26:38 +0800

    feat: add prod publicPath name

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,9 @@ const webpack = require('webpack');
 module.exports = defineConfig({
   transpileDependencies: true,
   // 佈署 https://tzuhui.io/2020/03/02/Vue/Vue-CLI3-to-github-pages/
-  publicPath: '/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue-shopping/'
+    : '/',
   configureWebpack: {
     plugins: [
       new webpack.ProvidePlugin({
