65e476af779b8b08eb20205fbd71e01526690a97
JessieRabbit <a0915655212@gmail.com>
2024-04-26 16:11:05 +0800

    feat: add prod publicPath name

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ const webpack = require('webpack');
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-shopping/'
+    ? '/fruitables/'
     : '/',
   configureWebpack: {
     plugins: [
