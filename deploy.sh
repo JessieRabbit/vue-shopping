6325ac283b6d57ca2b1b951c4e6c972a4b708c4e
JessieRabbit <a0915655212@gmail.com>
2024-04-26 15:46:19 +0800

    feat: add yarn deploy command line

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/JessieRabbit/vue-dashboard-record.git master:gh-pages
+git push -f https://github.com/JessieRabbit/vue-shopping.git master:gh-pages
 
 cd -
